10c85d9c768a2a94fdd6bdf9d8bd6a043056cd08
bradley-us <bradleydev101@gmail.com>
2022-05-08 11:03:47 +0100

    Loaded Img Files

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,23 +35,24 @@ const Home: NextPage = () => {
 
       <div>
         <div>
-          <div>
-            <div className='flex flex-col'>
+          <div className='lg:container mx-auto flex flex-row justify-between'>
+            <div className='mt-[-170px] flex flex-col'>
               <div className='flex justify-center items-center py-1 rounded-t-xl text-[#FF9900] font-bold bg-[#1A1A1A] w-32'>
                 <span>Marketing</span>
               </div>
-              <div>
-                <div>
-                  <div>
-                    <h1>Marketing Digital</h1>
+
+              <div className='rounded-tr-xl rounded-b-xl px-14 pt-8 pb-12 max-w-[600px] bg-[#242424]'>
+                <div className='flex flex-col gap-8'>
+                  <div className='flex flex-row justify-between items-start'>
+                    <h1 className='py-2 text-6xl font-bold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-myBlue to-myCian'>Marketing Digital</h1>
                     <figure>
-                      <Image src='/assets/img/marketingImgs/statistics.png' width={100} height={100} alt=''></Image>
+                      <Image src='/assets/img/marketingImgs/statistics.png' width={120} height={120} alt=''></Image>
                     </figure>
                   </div>
 
-                  <div>
-                    <hr className='w-2 h-[130px] bg-slate-200 border-slate-200 border-2 rounded-xl' />
-                    <p className='text-slate-200'>
+                  <div className='flex flex-row justify-between items-center gap-5'>
+                    <hr className='w-3 h-[130px] bg-slate-200 border-slate-200 border-4 rounded-xl' />
+                    <p className='text-slate-300 text-xl'>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iusto accusantium obcaecati tempora, blanditiis totam dicta, nulla veniam aperiam rerum maiores laboriosam esse atque doloribus deserunt itaque dignissimos quia. Nihil.
                     </p>
                   </div>
@@ -59,10 +60,15 @@ const Home: NextPage = () => {
               </div>
             </div>
 
-            <div>
-
+            <div className='flex flex-col items-center justify-center gap-5 mt-32'>
+              <Image src='/assets/img/marketingImgs/diagram.png' width={250} height={250} alt=''></Image>
+              <div className='rounded-xl flex justify-center items-center py-4 px-7 max-w-[300px] bg-slate-300'>
+                <p className='text-center'>Lorem ipsum dolor, adipisicing elit. Earum consequatur incidun.</p>
+              </div>
             </div>
           </div>
+
+          <div className='ghola'></div>
         </div>
       </div>
     </div>

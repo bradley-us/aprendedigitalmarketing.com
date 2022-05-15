18e47f1114c358c8424b72ff5a7c110a35d4f238
bradley-us <bradleydev101@gmail.com>
2022-05-15 14:58:40 +0100

    Web completed v1.1

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,15 +7,15 @@ const Header = (): JSX.Element => {
       <figure>
         <Image className='rounded-full' src="/assets/img/logos/logo_1.jpg" width={50} height={50} alt='Logo'></Image>
       </figure>
-      <nav className='inline-flex flex-row justify-around items-center gap-5'>
-        <ul className='font-bold flex flex-row justify-around items-center gap-2'>
-          <li><Link href={`#homeInit`}><a className='hover:bg-slate-200 hover:text-slate-800 transition ease-in duration-300 cursor-pointer py-2 px-5 rounded-xl'>Inicio</a></Link></li>
-          <li><Link href={`#aboutSection`}><a className='hover:bg-slate-200 hover:text-slate-800 transition ease-in duration-300 cursor-pointer py-2 px-5 rounded-xl'>Sobre mí</a></Link></li>
+      <nav className='inline-flex flex-row justify-around items-center gap-2'>
+        <ul className='font-bold flex flex-row justify-around items-center'>
+          <li><Link href={`#homeInit`}><a className='hover:bg-slate-200 hover:text-slate-800 transition ease-in duration-300 cursor-pointer py-2 px-3 rounded-xl'>Inicio</a></Link></li>
+          <li><Link href={`#aboutSection`}><a className='hover:bg-slate-200 hover:text-slate-800 transition ease-in duration-300 cursor-pointer py-2 px-3 rounded-xl'>Sobre mí</a></Link></li>
           {/* <li><Link href={`#`}><a>Contact</a></Link></li> */}
           {/* <li><Link href={`#`}><a>Servicios</a></Link></li> */}
         </ul>
         <div>
-          <Link href={'#toAction'}><a className='hover:bg-slate-200 transition ease-in duration-300 cursor-pointer text-gray-800 text-md font-bold bg-[#26EFEF] py-2 px-5 rounded-xl'>ACTION</a></Link>
+          <Link href={'#toAction'}><a className='hover:bg-slate-200 transition ease-in duration-300 cursor-pointer text-gray-800 text-md font-bold bg-[#26EFEF] py-2 px-3 rounded-xl'>¡Empezar!</a></Link>
         </div>
       </nav>
     </header>
